3c43869ecfd5d35e6d9d23b263b4886d883fbab3
Giles Papworth <990812+djsuperchief@users.noreply.github.com>
2021-01-20 07:47:00 +0000

    #30 Made headers immutable

---
+++ tests/UnitTests/Kyameru.Tests/EntityTests/RoutableTests.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Kyameru.Core.Entities;
+using NUnit.Framework;
+
+namespace Kyameru.Tests.EntityTests
+{
+    [TestFixture]
+    public class RoutableTests
+    {
+        [Test]
+        public void CreatedHeaderError()
+        {
+            Routable routable = this.CreateMessage();
+            Assert.Throws<Kyameru.Core.Exceptions.CoreException>(() => routable.SetHeader("Test", "changed"));
+        }
+
+        [Test]
+        public void UserImmutableThrowsHeader()
+        {
+            Routable routable = this.CreateMessage();
+            routable.SetHeader("&Nope", "Nope");
+            Assert.Throws<Kyameru.Core.Exceptions.CoreException>(() => routable.SetHeader("Nope", "yep"));
+        }
+
+        [Test]
+        public void UserMutableWorks()
+        {
+            Routable routable = this.CreateMessage();
+            routable.SetHeader("FileType", "txt");
+            routable.SetHeader("FileType", "jpg");
+            Assert.AreEqual("jpg", routable.Headers["FileType"]);
+        }
+
+        private Routable CreateMessage()
+        {
+            return new Routable(new System.Collections.Generic.Dictionary<string, string>()
+            {
+                {"Test", "value" }
+            }, "test");
+        }
+    }
+}
