909a2ae6ca0d31e80250dc4d9908e2d84d52ef31
Giles Papworth <990812+djsuperchief@users.noreply.github.com>
2021-10-20 20:02:38 +0100

    #55 Added slack component and updated for DI

---
+++ source/components/Kyameru.Component.Slack/Implementation/Payload.cs
@@ -0,0 +1,23 @@
+﻿namespace Kyameru.Component.Slack
+{
+    /// <summary>
+    /// Slack message payload
+    /// </summary>
+    internal class Payload
+    {
+        /// <summary>
+        /// Gets or sets the slack message.
+        /// </summary>
+        public string text { get; set; }
+
+        /// <summary>
+        /// Gets or sets the slack username to use.
+        /// </summary>
+        public string username { get; set; }
+
+        /// <summary>
+        /// Gets or sets the slack channel to use.
+        /// </summary>
+        public string channel { get; set; }
+    }
+}
---
+++ source/components/Kyameru.Component.Slack/Tests/InflatorTests.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Moq;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+
+namespace Kyameru.Component.Slack.Tests
+{
+    [TestFixture]
+    public class InflatorTests
+    {
+        private readonly Mock<ILogger<Kyameru.Route>> logger = new Mock<ILogger<Route>>();
+        
+        [Test]
+        public void ActivateToWorks()
+        {
+            Dictionary<string, string> headers = new Dictionary<string, string>()
+            {
+                { "Target", "test" },
+                { "MessageSource", "Body" }
+            };
+            Slack.Inflator inflator = new Inflator();
+            Assert.NotNull(inflator.CreateToComponent(headers, this.GetServiceProvider()));
+        }
+
+        [Test]
+        public void ActivateFromThrows()
+        {
+            Dictionary<string, string> headers = new Dictionary<string, string>()
+            {
+                { "Target", "test" }
+            };
+            Slack.Inflator inflator = new Inflator();
+            Assert.Throws<NotImplementedException>(() => { inflator.CreateFromComponent(headers, false, this.GetServiceProvider()); });
+        }
+
+        [Test]
+        public void ActivateAtomicThrows()
+        {
+            Dictionary<string, string> headers = new Dictionary<string, string>()
+            {
+                { "Target", "test" },
+                { "MessageSource", "Body" }
+            };
+            Slack.Inflator inflator = new Inflator();
+            Assert.Throws<NotImplementedException>(() => { inflator.CreateAtomicComponent(headers); });
+        }
+        private IServiceCollection GetServiceDescriptors()
+        {
+
+            IServiceCollection serviceDescriptors = new ServiceCollection();
+            serviceDescriptors.AddTransient<ILogger<Kyameru.Route>>(sp =>
+            {
+                return this.logger.Object;
+            });
+
+            Inflator inflator = new Inflator();
+            inflator.RegisterTo(serviceDescriptors);
+
+            return serviceDescriptors;
+        }
+
+        private IServiceProvider GetServiceProvider()
+        {
+            return this.GetServiceDescriptors().BuildServiceProvider();
+        }
+
+
+
+    }
+}
