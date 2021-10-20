4e0049f1db2a8a27a48290b743a7593e59cb6ee1
Giles Papworth <990812+djsuperchief@users.noreply.github.com>
2021-10-20 20:26:05 +0100

    #55 Quick console app for testing

---
+++ tests/TestApp/Kyameru.Console.Test/ProcessingComp.cs
@@ -0,0 +1,17 @@
+﻿using Kyameru.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Kyameru.Console.Test
+{
+    internal class ProcessingComp : IProcessComponent
+    {
+        public event EventHandler<Log> OnLog;
+
+        public void Process(Routable routable)
+        {
+            routable.SetBody<string>("Farts are not food");
+        }
+    }
+}
---
+++ tests/TestApp/Kyameru.Console.Test/Program.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Threading.Tasks;
+
+namespace Kyameru.Console.Test
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            await new HostBuilder().ConfigureServices((hostContext, services) =>
+            {
+                services.AddLogging();
+
+                Kyameru.Route.From("file:///c:/Temp?Notifications=Created&SubDirectories=true&Filter=*.*")
+                .Process(new ProcessingComp())
+                .To("slack:///TH7DAGRQE/B01DWN0088P/f23IiCaHrx8NnThl7xUDiDSn?MessageSource=Body&Channel=general&Username=Kyameru")
+                .Build(services);
+
+            }).ConfigureLogging((hostContext, services) =>
+            {
+                services.AddConsole();
+            }).RunConsoleAsync();
+
+
+        }
+    }
+}
