95b14e29817565805c2a46ce57d241365d7c7388
Giles Papworth <990812+djsuperchief@users.noreply.github.com>
2024-04-25 09:02:27 +0100

    #142 added full test

--- source/components/Kyameru.Component.SQS/Tests/FullTests.cs
+++ source/components/Kyameru.Component.SQS/Tests/FullTests.cs
@@ -65,9 +65,43 @@ public class FullTests
         await Task.CompletedTask;
     }
 
-    private IServiceCollection GetServiceDescriptors()
+    [Fact]
+    public async Task FullFromAndToWorksAsExpected()
+    {
+        var expectedMessage = Guid.NewGuid().ToString("N");
+        var serviceCollection = GetServiceDescriptors(expectedMessage);
+        var routable = new Routable(new Dictionary<string, string>(), "Test");
+        var extractor = Substitute.For<IExtractor>();
+        var syncEvent = new AutoResetEvent(false);
+
+        extractor.When(x => x.SetRoutable(Arg.Any<Routable>())).Do(x =>
+        {
+            routable = x[0] as Routable;
+        });
+        serviceCollection.AddTransient<IExtractor>(x => extractor);
+        Kyameru.Route.From("sqs://queue?PollTime=2")
+            .To("sqs://queue")
+            .To("faker://who/cares")
+            .Id("FakerSyncTest")
+            .BuildAsync(serviceCollection);
+        IServiceProvider provider = serviceCollection.BuildServiceProvider();
+        IHostedService service = provider.GetService<IHostedService>();
+        await service.StartAsync(CancellationToken.None);
+        syncEvent.WaitOne(5000);
+        await service.StopAsync(CancellationToken.None);
+
+        Assert.Equal("Faker Test", routable.Headers["SQSMessageId"]);
+        Assert.Equal("MessageHeader", routable.Headers["Full Test"]);
+        Assert.Equal(expectedMessage, routable.Body as string);
+    }
+
+    private IServiceCollection GetServiceDescriptors(string message = "Test")
     {
         var sqsClient = Substitute.For<IAmazonSQS>();
+        var messages = new List<string>()
+        {
+            { message }
+        };
         sqsClient.SendMessageAsync(Arg.Any<SendMessageRequest>(), Arg.Any<CancellationToken>()).Returns(x =>
         {
             var response = new SendMessageResponse();
@@ -76,6 +110,42 @@ public class FullTests
             Thread.Sleep(1000);
             return response;
         });
+
+        sqsClient.ReceiveMessageAsync(Arg.Any<ReceiveMessageRequest>()).Returns(x =>
+        {
+            if (messages.Count > 0)
+            {
+
+                var randomMessage = messages.First();
+                var response = Task.FromResult(new ReceiveMessageResponse()
+                {
+                    Messages = new List<Message>()
+                    {
+                        new()
+                        {
+                            MessageId = "Test",
+                            Body = randomMessage,
+                            ReceiptHandle = randomMessage,
+                            MessageAttributes = new Dictionary<string, MessageAttributeValue>()
+                            {
+                                { "Full Test", new () { DataType = "String", StringValue = "MessageHeader" } }
+                            }
+                        }
+                    }
+                });
+                return response;
+            }
+
+            return Task.FromResult(new ReceiveMessageResponse());
+        });
+
+
+        sqsClient.DeleteMessageAsync(Arg.Any<string>(), Arg.Any<string>(), Arg.Any<CancellationToken>()).Returns(x =>
+        {
+            messages.RemoveAt(messages.IndexOf(x[1] as string));
+            return Task.FromResult(new DeleteMessageResponse());
+        });
+
         IServiceCollection serviceCollection = new ServiceCollection();
         serviceCollection.AddTransient<ILogger<Kyameru.Route>>(sp =>
         {
