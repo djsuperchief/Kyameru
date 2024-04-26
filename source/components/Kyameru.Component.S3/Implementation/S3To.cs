8fe26a97107320a740c4a17d7a2a6f784d21269e
Giles Papworth <990812+djsuperchief@users.noreply.github.com>
2024-04-26 17:25:29 +0100

    #142 nullable reference types revert. Will fix later

--- source/components/Kyameru.Component.S3/Implementation/S3To.cs
+++ source/components/Kyameru.Component.S3/Implementation/S3To.cs
@@ -10,10 +10,10 @@ namespace Kyameru.Component.S3;
 public class S3To : ITo
 {
     private IAmazonS3 s3client;
-    private string targetBucket = "";
-    private string targetPath = "";
-    private string targetFileName = "";
-    private string targetContentType = "";
+    private string targetBucket;
+    private string targetPath;
+    private string targetFileName;
+    private string targetContentType;
 
     private readonly Dictionary<S3FileTarget.OperationType, Func<S3FileTarget, CancellationToken, Task<string>>> targetActions;
 
