66949a732f646acbe1f62d605c53881a2cf788bb
Giles Papworth <990812+djsuperchief@users.noreply.github.com>
2024-05-15 17:31:14 +0100

    #160 started initial swap of error messages

---
+++ source/core/Kyameru.Core/Exceptions/RouteNotAvailableException.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Kyameru.Core
+{
+    /// <summary>
+    /// Exception thrown when a Kyameru route is not implemented.
+    /// </summary>
+    public class RouteNotAvailableException : Exception
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="RouteNotAvailableException"/> class.
+        /// </summary>
+        /// <param name="message">Error Message.</param>
+        public RouteNotAvailableException(string message) : base(message)
+        {
+
+        }
+
+    }
+}
