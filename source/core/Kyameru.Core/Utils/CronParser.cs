9a898716ceeffa3bd23bf54a631c757695c72bda
Giles Papworth <990812+djsuperchief@users.noreply.github.com>
2024-09-16 17:28:42 +0100

    #177 latest cron tests TDD this one

--- source/core/Kyameru.Core/Utils/CronParser.cs
+++ source/core/Kyameru.Core/Utils/CronParser.cs
@@ -7,8 +7,9 @@ namespace Kyameru.Core.Utils;
 sealed class CronParser
 {
     // Mine so far: ^(^[0-9]$)|([1-5][0-9])|^\*$
-    private static readonly Regex minutesHoursRegex = new Regex(@"^((\d+,)+\d+|(\d+(-)\d+)|\d+|\*)$", RegexOptions.Compiled);
-    private static readonly char[] allowedChars = ['*', ',', '-'];
+    private static readonly Regex allowedRegex = new Regex(@"^((\d+,)+\d+|(\d+(-)\d+)|\d+|\*)$", RegexOptions.Compiled);
+    private static readonly string[] allowedMonths = ["JAN", "FEB", "MAR", "APR", "MAY", "JUN", "JUL", "AUG", "SEP", "OCT", "NOV", "DEC"];
+
     public static (bool, Entities.Cron) ValidateCron(string cron)
     {
         var isValid = CronIsValid(cron.Split(" "));
@@ -22,14 +23,43 @@ sealed class CronParser
 
     private static bool CronIsValid(string[] cron)
     {
-        return ValidateMinutes(cron[0]) && ValidateHours(cron[1]);
+        return ValidateMinutes(cron[0])
+            && ValidateHours(cron[1])
+            && ValidateDays(cron[2])
+            && ValidateMonth(cron[3])
+            && cron.Length == 5;
+    }
+
+    private static bool ValidateMonth(string month)
+    {
+        try
+        {
+            return ValidateRange(1, 12, month, allowedRegex)
+                || ValidateMatchedStrings(allowedMonths, month);
+        }
+        catch
+        {
+            return false;
+        }
+    }
+
+    private static bool ValidateDays(string day)
+    {
+        try
+        {
+            return ValidateRange(1, 31, day, allowedRegex);
+        }
+        catch
+        {
+            return false;
+        }
     }
 
     private static bool ValidateMinutes(string minutes)
     {
         try
         {
-            return ValidateRange(0, 59, minutes, minutesHoursRegex);
+            return ValidateRange(0, 59, minutes, allowedRegex);
 
         }
         catch
@@ -42,12 +72,60 @@ sealed class CronParser
     {
         try
         {
-            return ValidateRange(0, 23, hours, minutesHoursRegex);
+            return ValidateRange(0, 23, hours, allowedRegex);
         }
         catch
         {
             return false;
         }
+    }
+
+    private static bool ValidateMatchedStrings(string[] items, string expression)
+    {
+        var test = (string expression) =>
+        {
+            return expression.Trim().Length == 3 && items.Any(x => x == expression.Trim());
+        };
+
+        if (expression.Contains("-"))
+        {
+            var strings = expression.Split("-");
+            if (strings.Length > 2)
+            {
+                return false;
+            }
+
+            foreach (var item in strings)
+            {
+                if (!test(item))
+                {
+                    return false;
+                }
+            }
+
+            if (Array.IndexOf(items, strings[0]) >= Array.IndexOf(items, strings[1]))
+            {
+                return false;
+            }
+        }
+
+        if (expression.Contains(","))
+        {
+            foreach (var item in expression.Split(","))
+            {
+                if (!test(item))
+                {
+                    return false;
+                }
+            }
+        }
+
+        if (!expression.Contains(",") && !expression.Contains("-"))
+        {
+            return expression.Trim().Length == 3 && items.Any(x => x == expression.Trim());
+        }
+
+        return true;
     }
 
     private static bool ValidateRange(int min, int max, string expression, Regex regex)
@@ -56,8 +134,13 @@ sealed class CronParser
         if (!regex.IsMatch(expression)) return false;
         if (expression.Contains("-"))
         {
-            var numbers = expression.Split('-').Select(x => int.Parse(x));
+            var numbers = expression.Split('-').Select(x => int.Parse(x)).ToArray();
             if (numbers.Any(x => x > max || x < min))
+            {
+                return false;
+            }
+
+            if (numbers[0] >= numbers[1])
             {
                 return false;
             }
@@ -65,7 +148,7 @@ sealed class CronParser
 
         if (expression.Contains(","))
         {
-            var numbers = expression.Split(',').Select(x => int.Parse(x.Trim()));
+            var numbers = expression.Split(',').Select(x => int.Parse(x.Trim())).ToArray();
             if (numbers.Any(x => x > max || x < min))
             {
                 return false;
--- tests/UnitTests/Kyameru.Tests/UtilsTests/CronParserTests.cs
+++ tests/UnitTests/Kyameru.Tests/UtilsTests/CronParserTests.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Text;
 using Kyameru.Core.Utils;
 using NSubstitute;
 using Xunit;
@@ -12,6 +13,7 @@ public class CronParserTests
     [Theory]
     [InlineData("test", false)]
     [InlineData("0 1 * * *", true)]
+    [InlineData("0 1 * * * *", false)]
     public void IsValidCron(string cron, bool valid)
     {
         var (isValid, result) = CronParser.ValidateCron(cron);
@@ -39,6 +41,32 @@ public class CronParserTests
         Assert.Equal(isValid, CronParser.ValidateCron(cron).Item1);
     }
 
+    [Theory]
+    [MemberData(nameof(MonthOfYearTestData))]
+    public void MonthOfYearIsValid(string cron, bool isValid)
+    {
+        Assert.Equal(isValid, CronParser.ValidateCron(cron).Item1);
+    }
+
+    [Theory]
+    [MemberData(nameof(MonthStringTestData))]
+    public void MonthOfYearStringIsValid(string cron, bool isValid)
+    {
+        Assert.Equal(isValid, CronParser.ValidateCron(cron).Item1);
+    }
+
+    [Theory]
+    [MemberData(nameof(CommaTestData))]
+    public void GeneralCommaIsValid(string cron)
+    {
+        Assert.True(CronParser.ValidateCron(cron).Item1);
+    }
+
+    public static IEnumerable<object[]> MonthOfYearTestData()
+    {
+        return GenerateRangeTestData(1, 13, "* * * {0} *");
+    }
+
     public static IEnumerable<object[]> DayOfMonthTestData()
     {
         return GenerateRangeTestData(1, 32, "* * {0} * *");
@@ -51,12 +79,40 @@ public class CronParserTests
 
     public static IEnumerable<object[]> MinuteTestData()
     {
-        return GenerateRangeTestData(0, 60, "{0} * * *");
+        return GenerateRangeTestData(0, 60, "{0} * * * *");
+    }
+
+    public static IEnumerable<object[]> MonthStringTestData()
+    {
+        string[] months = ["JAN", "FEB", "MAR", "APR", "MAY", "JUN", "JUL", "AUG", "SEP", "OCT", "NOV", "DEC"];
+        for (var i = 0; i < months.Length; i++)
+        {
+            yield return new object[] { $"* * * {months[i]} *", true };
+        }
+        yield return new object[] { $"* * * TES *", false };
+        yield return new object[] { $"* * * TEST *", false };
+        for (var i = 1; i < months.Length; i++)
+        {
+            for (var x = 1; x < months.Length; x++)
+            {
+                bool isValid = x > i;
+                yield return new object[] { string.Format("* * * {0} *", $"{months[i]}-{months[x]}"), isValid };
+            }
+        }
+
+        for (var i = 1; i < months.Length; i++)
+        {
+            for (var x = 1; x < months.Length; x++)
+            {
+                bool isValid = true;
+                yield return new object[] { string.Format("* * * {0} *", $"{months[i]},{months[x]}"), isValid };
+            }
+        }
     }
 
     public static IEnumerable<object[]> GenerateRangeTestData(int min, int max, string expression)
     {
-        for (var i = min; i < max; i++)
+        for (var i = min; i <= max; i++)
         {
             var valid = i < max;
             yield return new object[] { string.Format(expression, i), valid };
@@ -65,14 +121,51 @@ public class CronParserTests
 
         for (var i = min; i < max; i++)
         {
-            for (var x = i + 1; x < max; x++)
+            for (var x = min; x < max; x++)
             {
                 var valid = i < max && x < max;
+                if (valid)
+                {
+                    valid = x > i;
+                }
+
                 yield return new object[] { string.Format(expression, $"{i}-{x}"), valid };
+            }
+        }
+
+        for (var i = min; i < max; i++)
+        {
+            for (var x = 0; x < max; x++)
+            {
+                var valid = i < max && x < max && i >= min && x >= min;
+                yield return new object[] { string.Format(expression, $"{i},{x}"), valid };
             }
         }
 
         yield return new object[] { string.Format(expression, $"{min},{max - 1}"), true };
         yield return new object[] { string.Format(expression, $"{min},{max}"), false };
+    }
+
+    public static IEnumerable<object[]> CommaTestData()
+    {
+
+        for (var position = 0; position < 6; position++)
+        {
+            var builder = new StringBuilder();
+            for (var i = 0; i < 6; i++)
+            {
+                if (i == position)
+                {
+                    builder.Append("1,2,3,5,4 ");
+                }
+                else
+                {
+                    builder.Append("* ");
+                }
+            }
+
+            yield return new object[] { builder.ToString().TrimEnd() };
+
+        }
     }
 }
