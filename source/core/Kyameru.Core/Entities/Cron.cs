8c4feafb3e0726b8aedaf542e9b2bd70e63f4da9
Giles Papworth <990812+djsuperchief@users.noreply.github.com>
2024-11-19 08:56:30 +0000

    shelving for now

--- source/core/Kyameru.Core/Entities/Cron.cs
+++ source/core/Kyameru.Core/Entities/Cron.cs
@@ -29,6 +29,11 @@ public class Cron
         { DateUnits.Day, 864000000000 }
     };
 
+    private readonly Dictionary<DateUnits, DateTimeMinMax> dateMinMax = new Dictionary<DateUnits, DateTimeMinMax>()
+    {
+        { DateUnits.Minute, new DateTimeMinMax() { Min = 0, Max = 59 }}
+    };
+
     private readonly string[] _cron;
 
     public Cron(string[] cron)
@@ -37,7 +42,7 @@ public class Cron
         _cron = cron;
         NextExecution = TimeProvider.Current.UtcNow;
         DateKindConfig = NextExecution.Kind;
-        CalculateNext();
+        //sCalculateNext();
     }
 
     public static Cron Create(string cron)
@@ -48,31 +53,49 @@ public class Cron
 
     public void Next()
     {
-        CalculateNext();
-    }
-
-    private void CalculateNext()
-    {
-        CalculateMinute();
-    }
-
-    private void CalculateMinute()
-    {
-        if (_cron[0] == "*")
+        foreach (var unit in dateMinMax.Keys)
         {
-            NextExecution = IncreaseTime(1, DateUnits.Minute);
+            var selectedCron = _cron[dateMinMax[unit].Index];
+            CalculateNextUnit(selectedCron, unit, dateMinMax[unit]);
+        }
+    }
+    private void CalculateNextUnit(string cron, DateUnits dateUnits, DateTimeMinMax dateTimeMinMax)
+    {
+        if (cron == "*")
+        {
+            NextExecution = NextSingle(dateUnits);
         }
 
-        if (NumbersOnly.Match(_cron[0]).Success)
+        if (NumbersOnly.Match(cron).Success)
         {
-            NextExecution = IncreaseTime(1, DateUnits.Hour).SetMinute(int.Parse(_cron[0]));
+            NextExecution = IncreaseTime(1, DateUnits.Hour).SetMinute(int.Parse(cron));
         }
 
         if (_cron[0].Contains('-'))
         {
             var expression = _cron[0].Split('-').Select(x => int.Parse(x.Trim())).ToArray();
-            NextExecution = NextExecution.GetCronMinuteBetween(expression[0], expression[1]);
+            //NextExecution = NextExecution.GetCronMinuteBetween(expression[0], expression[1]);
         }
+    }
+
+    private DateTime NextSingle(DateUnits unit)
+    {
+        return IncreaseTime(1, unit);
+    }
+
+    private DateTime GetNextAt(DateUnits unit, int next)
+    {
+        return IncreaseTime(next, unit);
+    }
+
+    private void CalcRange(int value, int range, DateTimeMinMax minMax)
+    {
+        if (value < minMax.Min || value > minMax.Max)
+        {
+            // throw an exception here.
+        }
+
+
     }
 
     private DateTime IncreaseTime(int amount, DateUnits unit)
---
+++ source/core/Kyameru.Core/Utils/DateTimeMinMax.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace Kyameru.Core.Utils;
+
+public class DateTimeMinMax
+{
+    public int Min { get; set; } = 0;
+
+    public int Max { get; set; } = 0;
+
+    public int Index { get; set; } = 0;
+}
