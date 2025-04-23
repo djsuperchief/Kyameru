5fa069b06a9c7d095406cad69231bc731001207c
Giles Papworth <990812+djsuperchief@users.noreply.github.com>
2025-04-23 17:09:44 +0100

    Resource string updates
    Clarification of resource strings
    
    [Ticket: #189]

--- source/core/Kyameru.Core/Resources.cs
+++ source/core/Kyameru.Core/Resources.cs
@@ -6,21 +6,21 @@
     public static class Resources
     {
         internal const string INFO_SETTINGUPROUTE = "Setting up route...";
-        internal const string INFO_PROCESSINGCOMPONENT = "Setting up processing component {0}...";
-        internal const string INFO_SETUP_TO = "Setting up to component {0}...";
-        internal const string INFO_SETUP_ERR = "Setting up error component {0}...";
-        internal const string INFO_SETUP_ATOMIC = "Setting up atomic component {0}...";
+        internal const string INFO_PROCESSINGCOMPONENT = "Setting up processor {0}...";
+        internal const string INFO_SETUP_TO = "Setting up to chain link {0}...";
+        internal const string INFO_SETUP_ERR = "Setting up error processor {0}...";
+        internal const string INFO_SETUP_ATOMIC = "Setting up atomic chain link {0}...";
 
-        internal const string ERROR_ACTIVATION_FROM = "Error activating from component.";
-        internal const string ERROR_ACTIVATION_TO = "Error activating to component.";
-        internal const string ERROR_ACTIVATING_ATOMIC = "Error activating atomic component";
-        internal const string ERROR_FROM_COMPONENT = "Error in starting from component, see inner exception.";
+        internal const string ERROR_ACTIVATION_FROM = "Error activating from chain link.";
+        internal const string ERROR_ACTIVATION_TO = "Error activating to chain link.";
+        internal const string ERROR_ACTIVATING_ATOMIC = "Error activating atomic chain link";
+        internal const string ERROR_FROM_COMPONENT = "Error in starting from chain link, see inner exception.";
         internal const string ERROR_HEADER_IMMUTABLE = "Error in setting header, header already exists. Headers are immutable.";
         internal const string ERROR_HEADER_CALLBACK = "Error executing header callback. See inner exception.";
         internal const string ERROR_REGISTERING_SERVICES = "Error registering services. See inner exception.";
         internal const string ERROR_ROUTE_URI = "Error in constructing route from URI, see inner exception.";
         internal const string ERROR_HEADER_IMMUTABLE_ADDED = "Error adding immutable header {0}, already present.";
-        internal const string ERROR_SETUP_COMPONENT_INVOCATION = "Error activating component. No valid invocation.";
+        internal const string ERROR_SETUP_COMPONENT_INVOCATION = "Error activating processor. No valid invocation.";
 
         internal const string ERROR_SCHEDULE_NOTSUPPORTED = "Component '{0}' does not support scheduling";
 
