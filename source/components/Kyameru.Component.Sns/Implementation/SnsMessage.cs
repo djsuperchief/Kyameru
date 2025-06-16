6f75058d83f4f3f28236bee0eda85957cce8def3
Giles Papworth <990812+djsuperchief@users.noreply.github.com>
2025-06-16 17:36:02 +0100

    Changed SNS to .NET Standard
    Over thought this one to be honest, I thought there was more than one component.
    
    [Ticket: #199]

--- source/components/Kyameru.Component.Sns/Implementation/SnsMessage.cs
+++ source/components/Kyameru.Component.Sns/Implementation/SnsMessage.cs
@@ -1,25 +1,28 @@
-﻿using Kyameru.Core.Entities;
+﻿using System.Collections.Generic;
+using System.Linq;
+using Kyameru.Core.Entities;
 
-namespace Kyameru.Component.Sns;
-
-public class SnsMessage
+namespace Kyameru.Component.Sns
 {
-    public string Message { get; private set; }
-
-    public string Arn { get; private set; }
-
-    public Dictionary<string, string> Attributes { get; private set; }
-
-    public static SnsMessage FromRoutable(Routable routable, Dictionary<string, string> componentHeaders)
+    public class SnsMessage
     {
-        var response = new SnsMessage
+        public string Message { get; private set; }
+
+        public string Arn { get; private set; }
+
+        public Dictionary<string, string> Attributes { get; private set; }
+
+        public static SnsMessage FromRoutable(Routable routable, Dictionary<string, string> componentHeaders)
         {
-            Message = routable.Body.ToString(),
-            Arn = routable.Headers.TryGetValue("SNSARN", componentHeaders["ARN"])
-        };
+            var response = new SnsMessage
+            {
+                Message = routable.Body.ToString(),
+                Arn = routable.Headers.TryGetValue("SNSARN", componentHeaders["ARN"])
+            };
 
-        response.Attributes = routable.Headers.ToDictionary(x => x.Key, x => x.Value);
+            response.Attributes = routable.Headers.ToDictionary(x => x.Key, x => x.Value);
 
-        return response;
+            return response;
+        }
     }
 }
--- source/components/Kyameru.Component.Sns/Implementation/SnsTo.cs
+++ source/components/Kyameru.Component.Sns/Implementation/SnsTo.cs
@@ -1,83 +1,89 @@
-﻿using System.Security.Cryptography.X509Certificates;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Threading;
+using System.Threading.Tasks;
 using Amazon.SimpleNotificationService;
 using Amazon.SimpleNotificationService.Model;
 using Kyameru.Core.Entities;
 using Microsoft.Extensions.Logging;
 
-namespace Kyameru.Component.Sns;
-
-public class SnsTo : ITo
+namespace Kyameru.Component.Sns
 {
-    public event EventHandler<Log> OnLog;
-
-    private readonly string[] requiredHeaders = ["ARN"];
-    private readonly IAmazonSimpleNotificationService snsClient;
-    private Dictionary<string, string> snsHeaders;
-
-    public SnsTo(IAmazonSimpleNotificationService client)
+    public class SnsTo : ITo
     {
-        snsClient = client;
-    }
+        public event EventHandler<Log> OnLog;
 
-    public async Task ProcessAsync(Routable routable, CancellationToken cancellationToken)
-    {
-        try
+        private readonly string[] requiredHeaders = new string[] { "ARN" };
+        private readonly IAmazonSimpleNotificationService snsClient;
+        private Dictionary<string, string> snsHeaders;
+
+        public SnsTo(IAmazonSimpleNotificationService client)
         {
-            Log(LogLevel.Information, Resources.INFO_PROCESSING);
-            var message = SnsMessage.FromRoutable(routable, snsHeaders);
-            var request = new PublishRequest
-            {
-                TopicArn = message.Arn,
-                Message = message.Message,
-                MessageDeduplicationId = routable.Headers.TryGetValue("SNSDeduplicationId", string.Empty),
-                MessageGroupId = routable.Headers.TryGetValue("SNSGroupId", string.Empty)
-            };
+            snsClient = client;
+        }
 
-            if (message.Attributes.Count > 0)
+        public async Task ProcessAsync(Routable routable, CancellationToken cancellationToken)
+        {
+            try
             {
-                request.MessageAttributes = message.Attributes.ToDictionary(x => x.Key, x => new MessageAttributeValue
+                Log(LogLevel.Information, Resources.INFO_PROCESSING);
+                var message = SnsMessage.FromRoutable(routable, snsHeaders);
+                var request = new PublishRequest
                 {
-                    DataType = "String",
-                    StringValue = x.Value
-                });
+                    TopicArn = message.Arn,
+                    Message = message.Message,
+                    MessageDeduplicationId = routable.Headers.TryGetValue("SNSDeduplicationId", string.Empty),
+                    MessageGroupId = routable.Headers.TryGetValue("SNSGroupId", string.Empty)
+                };
+
+                if (message.Attributes.Count > 0)
+                {
+                    request.MessageAttributes = message.Attributes.ToDictionary(x => x.Key, x => new MessageAttributeValue
+                    {
+                        DataType = "String",
+                        StringValue = x.Value
+                    });
+                }
+                Log(LogLevel.Information, Resources.INFO_SENDING);
+                var response = await snsClient.PublishAsync(request, cancellationToken);
+                if (response.HttpStatusCode == System.Net.HttpStatusCode.OK)
+                {
+                    routable.SetHeader("&SNSMessageId", response.MessageId);
+                    routable.SetHeader("&SNSSequenceNumber", response.SequenceNumber);
+                    Log(LogLevel.Information, string.Format(Resources.INFO_SENT, response.MessageId));
+                }
+                else
+                {
+                    var errorMessage = string.Format(Resources.ERROR_HTTP_RESPONSE, response.HttpStatusCode);
+                    Log(LogLevel.Information, errorMessage);
+                    routable.SetInError(new Error("SNS", "To", errorMessage));
+                }
             }
-            Log(LogLevel.Information, Resources.INFO_SENDING);
-            var response = await snsClient.PublishAsync(request, cancellationToken);
-            if (response.HttpStatusCode == System.Net.HttpStatusCode.OK)
+            catch (Exception ex)
             {
-                routable.SetHeader("&SNSMessageId", response.MessageId);
-                routable.SetHeader("&SNSSequenceNumber", response.SequenceNumber);
-                Log(LogLevel.Information, string.Format(Resources.INFO_SENT, response.MessageId));
-            }
-            else
-            {
-                var errorMessage = string.Format(Resources.ERROR_HTTP_RESPONSE, response.HttpStatusCode);
-                Log(LogLevel.Information, errorMessage);
-                routable.SetInError(new Error("SNS", "To", errorMessage));
+                routable.SetInError(new Error("SNS", "To", ex.Message));
+                Log(LogLevel.Error, string.Format(Resources.ERROR_SENDING, ex.Message), ex);
             }
         }
-        catch (Exception ex)
+        public void SetHeaders(Dictionary<string, string> headers)
         {
-            routable.SetInError(new Error("SNS", "To", ex.Message));
-            Log(LogLevel.Error, string.Format(Resources.ERROR_SENDING, ex.Message), ex);
-        }
-    }
-    public void SetHeaders(Dictionary<string, string> headers)
-    {
-        var missing = requiredHeaders.Where(x => headers.Keys.All(y => y != x));
-        if (missing.Any())
-        {
-            throw new Core.Exceptions.ComponentException(string.Format(Resources.MISSING_HEADER_EXCEPTION, string.Join(",", missing.ToList())));
+            var missing = requiredHeaders.Where(x => headers.Keys.All(y => y != x));
+            if (missing.Any())
+            {
+                throw new Core.Exceptions.ComponentException(string.Format(Resources.MISSING_HEADER_EXCEPTION, string.Join(",", missing.ToList())));
+            }
+
+            snsHeaders = headers;
         }
 
-        snsHeaders = headers;
-    }
-
-    private void Log(LogLevel logLevel, string message, Exception exception = null)
-    {
-        if (this.OnLog != null)
+        private void Log(LogLevel logLevel, string message, Exception exception = null)
         {
-            this.OnLog?.Invoke(this, new Core.Entities.Log(logLevel, message, exception));
+            if (this.OnLog != null)
+            {
+                this.OnLog?.Invoke(this, new Core.Entities.Log(logLevel, message, exception));
+            }
         }
     }
 }
