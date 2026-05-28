72aa5bbb0dcb5214d98a0db4638de4c75d2a8704
Giles Papworth <990812+djsuperchief@users.noreply.github.com>
2026-05-28 08:58:09 +0100

    Initial implementation of stream.
    Needs testing and logging.
    
    [Ticket: #214]

--- source/components/Kyameru.Component.DynamoDB/Implementation/DynamoDbFrom.cs
+++ source/components/Kyameru.Component.DynamoDB/Implementation/DynamoDbFrom.cs
@@ -2,11 +2,14 @@ using System;
 using System.Collections.Generic;
 using System.Threading;
 using System.Threading.Tasks;
-using System.Timers;
 using Amazon.DynamoDBStreams;
+using Amazon.DynamoDBStreams.Model;
 using Amazon.DynamoDBv2;
+using Amazon.DynamoDBv2.DocumentModel;
 using Kyameru.Component.DynamoDB.Contracts;
+using Kyameru.Component.DynamoDB.Extensions;
 using Kyameru.Core.Entities;
+using Kyameru.Core.Exceptions;
 using Kyameru.Core.Sys;
 
 namespace Kyameru.Component.DynamoDB
@@ -21,36 +24,34 @@ namespace Kyameru.Component.DynamoDB
         
         public event EventHandler<Log>? OnLog;
         public event AsyncEventHandler<RoutableEventData>? OnActionAsync;
-        private Dictionary<string, string> _headers;
+        private string _tableName;
         private readonly IAmazonDynamoDB _dynamoDbClient;
         private readonly IAmazonDynamoDBStreams _dynamoDbStreams;
         private CancellationToken _cancellationToken;
-        private System.Timers.Timer _poller;
+        private System.Threading.Timer _poller;
         private int _polltime;
         private bool _isStopping = false;
+        private AutoResetEvent _timerEvent = new AutoResetEvent(false);
+        private Dictionary<string, string> _headers;
 
         public void Setup()
         {
-            // TODO: validate headers.
-            _poller.Elapsed += Poller_Elapsed;
-            _poller.Interval = _polltime;
-            _poller.AutoReset = true;
+            ValidateHeaders();
+            
         }
+
 
         public async Task StartAsync(CancellationToken cancellationToken)
         {
             _cancellationToken = cancellationToken;
-            _poller.Start();
+            _poller = new Timer(TimerElapsed, _timerEvent, TimeSpan.FromSeconds(_polltime), TimeSpan.FromSeconds(_polltime));
             await Process(_cancellationToken);
         }
 
         public async Task StopAsync(CancellationToken cancellationToken)
         {
             _isStopping = true;
-            _poller.Stop();
-            _poller.Elapsed -= Poller_Elapsed;
-            
-            await Task.CompletedTask;
+            await _poller.DisposeAsync();
         }
 
         public void SetHeaders(Dictionary<string, string> headers)
@@ -58,21 +59,68 @@ namespace Kyameru.Component.DynamoDB
             _headers = headers;
         }
         
-        private void Poller_Elapsed(object sender, ElapsedEventArgs e)
+        private async void TimerElapsed(object state)
         {
             if (!_isStopping)
             {
-                // process
+                await Process(_cancellationToken);
             }
-            else
-            {
-                _poller.Stop();
-            }
+
+            await Task.CompletedTask;
         }
 
         private async Task Process(CancellationToken stoppingToken)
         {
+            var table = await _dynamoDbClient.DescribeTableAsync(_tableName, stoppingToken);
+            var stream = table.Table.LatestStreamArn;
+
+            var shards = await _dynamoDbStreams.DescribeStreamAsync(stream, stoppingToken);
+
+            foreach (var shard in shards.StreamDescription.Shards)
+            {
+                await ProcessShard(shard, stream, stoppingToken);
+            }
             
+        }
+
+        private async Task ProcessShard(Shard shard, string streamArn, CancellationToken stoppingToken)
+        {
+            var shardIteratorResponse = await _dynamoDbStreams.GetShardIteratorAsync(new GetShardIteratorRequest()
+            {
+                StreamArn = streamArn,
+                ShardId = shard.ShardId,
+                ShardIteratorType = ShardIteratorType.LATEST
+            }, stoppingToken);
+            
+            var iterator = shardIteratorResponse.ShardIterator;
+            var records = new List<string>();
+            while (!string.IsNullOrWhiteSpace(iterator) && !stoppingToken.IsCancellationRequested)
+            {
+                var recordsResponse = await _dynamoDbStreams.GetRecordsAsync(iterator, stoppingToken);
+                foreach (var record in recordsResponse.Records)
+                {
+                    records.Add(record.Dynamodb.Keys.ToJson());
+                }
+
+                iterator = recordsResponse.NextShardIterator;
+            }
+
+            var routable = new Routable(new Dictionary<string, string>(), records);
+            if (OnActionAsync != null)
+            {
+                await OnActionAsync.Invoke(this, new RoutableEventData(routable,  stoppingToken));
+            }
+        }
+
+
+        private void ValidateHeaders()
+        {
+            if (!_headers.TryGetValue("Host", out var header))
+            {
+                throw new MissingHeaderException(Resources.EXCEPTION_MISSINGTABLENAME);
+            }
+            
+            _tableName = header;
         }
     }
 }
